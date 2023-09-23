16fa7a24e873d405728d497c425de68f28939925
Alena Grinda <alenagrinda@gmail.com>
2023-09-24 00:55:09 +0700

    Книги

--- app.py
+++ app.py
@@ -159,4 +159,19 @@ def example():
         {'name':'мандарины', 'price': 95}, 
         {'name':'манго', 'price': 321} 
         ]
-    return render_template('example.html', name = name, group =  group, course = course, lab_num = lab_num, fruits=fruits)   
+    books = [
+        {'author': 'Лев Толстой', 'name': 'Война и Мир', 'genre': 'Историческая проза', 'pages': 1300},
+        {'author': 'Джордж Оруэлл', 'name': '1984', 'genre': 'Социальная фантастика', 'pages': 320},
+        {'author': 'Джеймс Джойс', 'name': 'Улисс', 'genre': 'Роман', 'pages': 800},
+        {'author': 'Владимир Набоков ', 'name': 'Лолита', 'genre': 'Роман', 'pages': 448},
+        {'author': 'Уильям Фолкнер', 'name': 'Звук и ярость', 'genre': 'Роман', 'pages': 416},
+        {'author': 'Ральф Эллисон', 'name': 'Человек-невидимка', 'genre': 'Роман', 'pages': 170},
+        {'author': 'Вирджиния Вулф', 'name': 'На маяк', 'genre': 'Роман', 'pages': 288},
+        {'author': 'Джейн Остен', 'name': 'Гордость и предубеждение', 'genre': 'Роман', 'pages': 416},
+        {'author': 'Данте Алигьери', 'name': 'Божественная комедия', 'genre': 'Поэма', 'pages': 464},
+        {'author': 'Джеффри Чосер', 'name': 'Кентерберийские рассказы', 'genre': 'Проза', 'pages': 800},
+
+
+
+    ]
+    return render_template('example.html', name = name, group =  group, course = course, lab_num = lab_num, fruits=fruits, books = books)   
