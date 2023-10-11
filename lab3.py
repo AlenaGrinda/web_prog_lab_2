6819318bf42ad9d36c99cd4055820302761e7025
Alena Grinda <alenagrinda@gmail.com>
2023-10-12 00:35:04 +0700

    Заготовка к третьей лабораторной работе

---
+++ lab3.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, redirect, url_for, render_template
+lab3 = Blueprint('lab3', __name__)
+
+@lab3.route('/lab3/')
+def lab():
+ return render_template('lab3.html') 
+
+
+@lab3.route('/lab3/form1')
+def form1():
+    return'''
+ 
+'''   
+
+
